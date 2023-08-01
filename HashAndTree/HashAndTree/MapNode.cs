9549dec1bdddcc20432b225cfff69608d79fce47
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-08-01 23:36:31 +0530

    [Mukesh] Add : Finding frequency of words in table

---
+++ HashAndTree/HashAndTree/MapNode.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashAndTree.HashAndTree
+{
+    internal class MapNode<M,N>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<M, N>>[] items;
+        public MapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<M, N>>[size];
+        }
+        public int GetArrayPosition(M key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public N Get(M key)
+        {
+            int postionnn = GetArrayPosition(key);
+            LinkedList<KeyValue<M, N>> LinkedList = GetLinkedList(postionnn);
+            foreach (KeyValue<M, N> item in LinkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(N);
+        }
+        public void Add(M key, N value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<M, N>> linkedList = GetLinkedList(position);
+            KeyValue<M, N> item = new KeyValue<M, N>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        public void Remove(M key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<M, N>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<M, N> foundItem = default(KeyValue<M, N>);
+            foreach (KeyValue<M, N> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+        protected LinkedList<KeyValue<M, N>> GetLinkedList(int postion)
+        {
+            LinkedList<KeyValue<M, N>> linkedList = items[postion];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<M, N>>();
+                items[postion] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+    public struct KeyValue<M, N>
+    {
+        public M Key { get; set; }
+        public N Value { get; set; }
+
+    }
+}
---
+++ HashAndTree/Program.cs
@@ -0,0 +1,26 @@
+﻿using HashAndTree.HashAndTree;
+using HashAndTree;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("HashTable and Binary tree!");
+        MapNode<string, string> hash = new MapNode<string, string>(5);
+        hash.Add("100", "hundred");
+        hash.Add("90", "ninty");
+        hash.Add("80", "eighty");
+        hash.Add("70", "seventy");
+        hash.Add("60", "sixty");
+        hash.Add("50", "fifty");
+        string hash5 = hash.Get("5");
+        Console.WriteLine("5th index value : " + hash5);
+        hash.Remove("2");
+        string hash2 = hash.Get("2");
+        Console.WriteLine("2th index value : " + hash2);
+
+
+
+
+    }
+}
