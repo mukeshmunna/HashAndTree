00ec6f7d16b9246ed0fb428c243dac3728963066
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-08-02 15:49:16 +0530

    [Mukesh] Add : created binary search tree

--- HashAndTree/Program.cs
+++ HashAndTree/Program.cs
@@ -1,45 +1,50 @@
 ﻿using HashAndTree.HashAndTree;
 using HashAndTree;
+using HashAndTrees;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        string statement = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-        string[] sentence = statement.Split(" ");
-        MapNode<string, string> hash = new MapNode<string, string>(sentence.Length);
-        for (int i = 0; i < sentence.Length; i++)
-        {
-            hash.Add(Convert.ToString(i), sentence[i]);
-        }
-        for (int i = 0; i < sentence.Length; i++)
-        {
-            int Count = 1;
-            for (int j = i + 1; j < sentence.Length; j++)
-            {
-                if (hash.Get(Convert.ToString(i)) == hash.Get(Convert.ToString(j)))
-                {
-                    Count++;
-                    sentence[j] = null;
-                }
-            }
-            if (sentence[i] != null)
-                Console.WriteLine("The Frequency of the Word " + sentence[i] + " is " + Count);
-        }
-        Console.WriteLine("Enter the word to be Removed");
-        string word = Console.ReadLine();
-            int count = 0;
-            for (int i = 0; i < sentence.Length; i++)
-            {
-                if (hash.Get(Convert.ToString(i)) == word)
-                {
-                    hash.Remove(Convert.ToString(i));
-                    count++;
-                }
-            }
-            if (count == 0)
-                Console.WriteLine("Enter the correct word to Remove");
-        
+        /* string statement = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+         string[] sentence = statement.Split(" ");
+         MapNode<string, string> hash = new MapNode<string, string>(sentence.Length);
+         for (int i = 0; i < sentence.Length; i++)
+         {
+             hash.Add(Convert.ToString(i), sentence[i]);
+         }
+         for (int i = 0; i < sentence.Length; i++)
+         {
+             int Count = 1;
+             for (int j = i + 1; j < sentence.Length; j++)
+             {
+                 if (hash.Get(Convert.ToString(i)) == hash.Get(Convert.ToString(j)))
+                 {
+                     Count++;
+                     sentence[j] = null;
+                 }
+             }
+             if (sentence[i] != null)
+                 Console.WriteLine("The Frequency of the Word " + sentence[i] + " is " + Count);
+         }
+         Console.WriteLine("Enter the word to be Removed");
+         string word = Console.ReadLine();
+             int count = 0;
+             for (int i = 0; i < sentence.Length; i++)
+             {
+                 if (hash.Get(Convert.ToString(i)) == word)
+                 {
+                     hash.Remove(Convert.ToString(i));
+                     count++;
+                 }
+             }
+             if (count == 0)
+                 Console.WriteLine("Enter the correct word to Remove");*/
+        BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+        binarySearch.Insert(30);
+        binarySearch.Insert(70);
+        binarySearch.Display();
+
     }
     
 }
